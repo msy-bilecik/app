d9d8ab9c983f52382f45b72b9d48bf8aae58a757
msy.bilecik <msy.bilecik@gmail.com>
2021-08-10 18:07:48 +0300

    mrcnn added

--- app.py
+++ app.py
@@ -1,47 +1,49 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
+from mrcnn.model import log
+import mrcnn.model as modellib
+from mrcnn.visualize import display_images
+from mrcnn import visualize
+from mrcnn import utils
+import configFile
+from py import metricQc
+from py import metreE
+from py import util
+from keras.preprocessing import image
+from keras.models import load_model
+from tensorflow import keras
 from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect, flash, abort
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 import numpy as np
 import pandas as pd
-from flask import jsonify
-from flask import render_template
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import load_model
-from keras.preprocessing import image
-from py import util
-from py import metreE
-from py import metricQc
-import configFile
-# from util import base64_to_pil, pil2datauri
 
 import os
 import sys
 import io
+import cv2
 
 from PIL import Image
-from mrcnn import utils
-from mrcnn import visualize
-from mrcnn.visualize import display_images
-import mrcnn.model as modellib
-from mrcnn.model import log
+import tensorflow as tf
+print(tf.__version__)
 
-#gpus = tf.config.experimental.list_physical_devices('GPU')
-#if gpus:
-#    try:
-#        # Currently, memory growth needs to be the same across GPUs
-#        for gpu in gpus:
-#            tf.config.experimental.set_memory_growth(gpu, True)
-#        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-#        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-#    except RuntimeError as e:
-#        # Memory growth must be set before GPUs have been initialized
-#        print(e)
+# from util import base64_to_pil, pil2datauri
 
 
-DEVICE = "/cpu:0"
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
+DEVICE = "/gpu:0"
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
@@ -51,17 +53,18 @@ MODEL_PATH = 'static/h5/msDet.h5'
 
 class InferenceConfig(config.__class__):
     # Run detection on one image at a time
-    GPU_COUNT = 0
+    GPU_COUNT = 1
     IMAGES_PER_GPU = 1
     ALLOW_GROWTH = True
     PER_PROCESS_GPU_MEMORY_FRACTION = 0.9
 
 
 config = InferenceConfig()
-# config.display()
+config.display()
 with tf.device(DEVICE):
     model = modellib.MaskRCNN(
         mode="inference", model_dir=MODEL_PATH, config=config)
+
 
 model.load_weights(MODEL_PATH, by_name=True)
 model.keras_model._make_predict_function()
@@ -118,86 +121,85 @@ def msDetection():
     return render_template('detection.html', title=title, cap=cap)
 
 
-@app.route('/msDetection/<fname>')
-def detecFile1():
-    if request.method == 'POST':
-        q = dict(request.files)
-        img = q["image"]
-        img = Image.open(io.BytesIO(img.read()))
-        # img = Image.open(io.BytesIO(img))
-        # img = base64_to_pil(request.json)
-        preds = model_predict(img, model)
-        if preds.size > 0:
-            print(preds)
-            responsejson = {
-                'x1': int(preds[0][0]),
-                'x2': int(preds[0][1]),
-                'y1': int(preds[0][2]),
-                'y2': int(preds[0][3]),
-                }
-            json_resp = jsonify(responsejson)
-            json_resp.status_code = 200
-            print(json_resp)
-            return json_resp
-        else:
-            json_resp = {
-                'x1': 'None',
-                'x2': 'None',
-                'y1': 'None',
-                'y2': 'None',
-                }
-            json_resp = jsonify(json_resp)
-            json_resp.status_code = 200
-    return json_resp
-    
-    #return render_template('detection.html', title=title, cap=cap)
-
-
+@app.route('/msDetection/<filename>')
 def detecFile(filename):
     #title = "MS Detection"
     #cap = "MS Detection - Test"
-    #return render_template('detection.html', title=title, cap=cap+" "+fname, filename=fname)
+    # return render_template('detection.html', title=title, cap=cap+" "+fname, filename=fname)
     return redirect(url_for('static', filename='uploadFolder/'+filename), code=301)
 
 
 @app.route('/upload1File', methods=['POST'])
 def upload1File():
     if request.method == 'POST':
+        # formdan dosya gelip gelmediğini kontrol edelim
+        if 'fname' not in request.files:
+            flash('Dosya seçilmedi')
+            return redirect('msDetection')
 
-		# formdan dosya gelip gelmediğini kontrol edelim
-      if 'fileMRi' not in request.files:
-         flash('Dosya seçilmedi')
-         return redirect('msDetection')
+            # kullanıcı dosya seçmemiş ve tarayıcı boş isim göndermiş mi
+        f = request.files['fname']
+        if f.filename == '':
+            flash('Dosya seçilmedi')
+            return redirect('msDetection')
 
-		# kullanıcı dosya seçmemiş ve tarayıcı boş isim göndermiş mi
-      f = request.files['fileMRi']
-      if f.filename == '':
-         flash('Dosya seçilmedi')
-         return redirect('msDetection')
-
-		# gelen dosyayı güvenlik önlemlerinden geçir
-      if f and uzanti_kontrol(f.filename):
-          filename = secure_filename(f.filename)
-          f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-          flash('Dosya başarıyla yüklendi.')
-          title = "MS Detection"
-          cap = "MS Detection - Test"
-          return render_template('detection.html', title=title, cap=cap+" "+filename, filename=filename)
-
-      else:
-         flash('İzin verilmeyen dosya uzantısı')
-         return redirect('msDetection')
+            # gelen dosyayı güvenlik önlemlerinden geçir
+        if f and uzanti_kontrol(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Dosya başarıyla yüklendi.')
+            title = "MS Detection"
+            cap = "MS Detection - Test"
+            result=segmentFile(filename)
+            return render_template('detection.html', title=title, cap=cap+" "+filename, filename=filename,result=result)
+        else:
+            flash('İzin verilmeyen dosya uzantısı')
+            return redirect('msDetection')
     else:
         abort(401)
+
+def segmentFile(filename):
+    message=""
+    #img = Image.open(io.BytesIO(img.read()))
+    #img = Image.open(io.BytesIO(img))
+    # img = base64_to_pil(request.json)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    img = cv2.imread(filepath)
+    preds = model_predict(img, model)
+    if preds.size > 0:
+        message="sss"
+        for pred in preds:
+            responsejson= {
+                    'x1': int(pred[0][0]),
+                    'x2': int(pred[0][1]),
+                    'y1': int(pred[0][2]),
+                    'y2': int(pred[0][3]),
+                }
+                #json_resp = jsonify(responsejson)
+                #json_resp.status_code = 200
+                #print(json_resp)
+        message=responsejson
+    else:
+        message="zzz"
+        json_resp = {
+                    'x1': 'None',
+                    'x2': 'None',
+                    'y1': 'None',
+                    'y2': 'None',
+                }
+                #json_resp = jsonify(json_resp)
+                #json_resp.status_code = 200
+                #print(json_resp)
+        message = json_resp
+    return message
 
 
 @app.route('/followup')
 def followup():
-   
+
     title = "MS FollowUp "
     cap = "MS FollowUp - Test"
     return render_template('main.html', title=title, cap=cap)
-
 
 
 @app.route('/about')
@@ -207,6 +209,5 @@ def about():
     return render_template('main.html', title=title, cap=cap)
 
 
-
-if __name__=="__main__":
-   app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(debug=True)
